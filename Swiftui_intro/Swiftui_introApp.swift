22acba79df7d9e21ef6b0509309a3e392d3e8fd9
philipmir <philipmir@hotmail.com>
2023-04-03 11:14:52 +0200

    Initial Commit

---
+++ Swiftui_intro/Swiftui_introApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_introApp.swift
+//  Swiftui_intro
+//
+//  Created by Philip Miranda on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
